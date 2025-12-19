28ddcb032380db08afb207945c64b3b052ec6dff
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-12-20 01:58:37 +0700

    register error handling

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs";
 import prisma from "@/libs/prisma";
+import { Prisma } from "@/generated/prisma/client";
 import * as z from "zod";
 
+// zod type validation
 const Player = z.object({
   username: z.string().min(5),
   password: z.string().min(5),
@@ -28,16 +30,33 @@ export async function POST(req: Request) {
   }
 
   // hash password
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(result.data.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(result.data.password, salt);
 
-  // push new user with hashed password to db
-  const users = await prisma.user.create({
-    data: { name: result.data.username, password: hash },
-  });
+  // push new user with hashed password to db and its (overkill imo) error handling
+  try {
+    const users = await prisma.user.create({
+      data: { name: result.data.username, password: hash },
+    });
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (err.code) {
+        case "P2002": // unique constraint
+          return Response.json(
+            { error: "Username already exists" },
+            { status: 409 },
+          );
+
+        default:
+          return Response.json(
+            { error: "Database error", code: err.code },
+            { status: 500 },
+          );
+      }
+    }
+  }
 
   return Response.json({
-    users,
     result,
   });
 }
